6ff577af07aebd452a9d1f530065656141156d7e
elionavarroh <elionavarroh@gmail.com>
2023-04-02 11:36:31 +0200

    add the basic structure of the shopping cart

---
+++ app.js
@@ -0,0 +1,30 @@
+const carrito = [
+    {
+    id: 198752,
+    name: "Tinta DJ27 Color",
+    price: 52.95,
+    count: 3,
+    premium: true
+    },
+    {
+    id: 75621,
+    name: "Impresora ticketera PRO-201",
+    price: 32.75,
+    count: 2,
+    premium: true
+    },
+    {
+    id: 54657,
+    name: "Caja de rollos de papel para ticketera",
+    price: 5.95,
+    count: 3,
+    premium: false
+    },
+    {
+    id: 3143,
+    name: "Caja de folios DIN-A4 80gr",
+    price: 9.95,
+    count: 2,
+    premium: false
+    }
+   ];
